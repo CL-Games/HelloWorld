3666ead6eb56995dcb5223664de500e3e9bf80e9
CL-Games <75060019+CL-Games@users.noreply.github.com>
2021-09-23 10:06:26 +0800

    Create MATH.C

---
+++ MATH.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    printf("Hello World");
+    printf("Hello World\n");
+    printf("%d\n",maxValue(5, 10));
+    printf("%d\n",maxValue(10, 5));
+    printf("%d\n",minValue(5, 10));
+    printf("%d\n",minValue(10, 5));
+    return 0;
+}
+int maxValue(int a, int b){
+    if(a > b){
+        return a;
+    }
+    else{
+      return b;
+    }
+}
+int minValue(int a, int b){
+    if(a < b){
+        return a;
+    }
+    else{
+      return b;
+    }
+} 
